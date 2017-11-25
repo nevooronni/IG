a773d565e5c9d998cb0014e9d6d378a28dcd46e5
Neville Oronni <nevooronni@gmail.com>
2017-11-25 23:21:24 +0300

    added static files and bootsttap

--- instagram/app/views.py
+++ instagram/app/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse#module and the class
+import datetime as dt
+from django.shortcuts import render,redirect
 
 #create your views here.views
-def welcome(request):
-	return HttpResponse('Welcome to the instagram app')
+def welcome(request):#one argument which is the request contains information fo the current web request that has triggered this view must be the first paremeter of our view functions instance of the django.http.HttpResponse class 
+	date = dt.date.today()#get current date
+
+	return render(request, 'all-app/index.html', {"date":date})
+
+# def post_of_today(request):
+# 	date = dt.date.today()#get current date
+	
+# 	#function to convert date object to find specific day
+# 	day = convert_dates(date)
+# 	html = f'''
+# 		<html>
+# 			<body>
+# 				<h1>instagram</h1>
+# 				<p>{day} {date.day}-{date.month}-{date.year}</p>
+# 			</body>
+# 		</html>
+# 					'''
+# 	return HttpResponse(html)
