f10185b55114b656563c5d9c5765ef889e1a86ed
Neville Oronni <nevooronni@gmail.com>
2017-11-26 18:00:05 +0300

    added modle admin class to edit our models better in admin page

--- instagram/app/admin.py
+++ instagram/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile,Tags,Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+	filter_horizontal = ('tags',)#specify the property allows ordering of many to many fields and pass in the tags article field
+	
+admin.site.register(Profile)
+admin.site.register(Tags)
+admin.site.register(Post,PostAdmin)
--- instagram/app/urls.py
+++ instagram/app/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url#import url function
 from . import views#import views module
 
 urlpatterns=[
-	url('^$',views.welcome,name = 'welcome'),
-	# url('^today/$',views.post_of_today,name = 'postToday')
+	url('^$',views.post_of_today,name = 'postToday')
 ]#list of url instances for our app which have a regular experession,view,kwargs,name(last two are optional)
