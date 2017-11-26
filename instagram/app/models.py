fe295792d1e0a4cb1791f5990933d0021c43f042
Neville Oronni <nevooronni@gmail.com>
2017-11-26 17:26:44 +0300

    created and run tests for all app models

--- instagram/app/models.py
+++ instagram/app/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save#define signals so our profile model auto created/updated when we create/update user instances.
+from django.dispatch import receiver
+import datetime as dt
 
-# Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)#USER Models
+	bio = models.TextField(max_length=500, blank=True)
+
+	def __str__(self):
+		return self.user.username
+
+	@classmethod
+	def retrieve_profiles(cls):
+		all_profiles = Profile.objects.all()
+		return all_profiles
+
+#hooking up the create_user_profile and save_user_profile methods to the User model wherever a save evernt occurs
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)#creates a profile when creating a user
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()#save a profile when saving a user
+
+
+class Tags(models.Model):
+	title = models.CharField(max_length=30, unique=True)
+
+	def __str__(self):
+		return self.title
+	class Meta:
+		ordering = ['title']#ordering data everytime can be tedious meta subclass to specify model-specific options 
+
+	def save_tag(self):
+		self.save()
+
+	def delete_tag(self):
+		self.delete()
+
+	@classmethod
+	def retrieve_tags(cls):
+		tags = Tags.objects.all()
+		return tags
+
+class Post(models.Model):
+	post_time = models.DateTimeField(auto_now_add=True)
+	tags = models.ManyToManyField(Tags, blank=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+	caption = models.TextField(blank=True)
+
+	def __str__(self):
+		return self.user.username
+	class Meta:
+		ordering = ['-post_time']#orders with the most recent at the top
+	
+	@classmethod
+	def retrieve_profile_posts(cls,profile_id):
+		prof_posts = Post.objects.filter(profile=profile_id).all()
+		return prof_posts
+
+	@classmethod
+	def retrieve_posts(cls):
+		posts = Post.objects.all()
+		return posts
--- instagram/app/tests.py
+++ instagram/app/tests.py
@@ -1,3 +1,119 @@
 from django.test import TestCase
+from django.contrib.auth.models import User#import user models 
+from .models import Tags,Profile,Post
 
-# Create your tests here.
+class ProfileTestClass(TestCase):
+	'''
+	test case for our profie class
+	'''
+	def setUp(self):
+		'''
+		setup method
+		'''
+		self.profile_one = Profile(bio = 'all in')#create an instance of the profile class for every test
+
+	def test_instance(self):
+		'''
+		test the method
+		'''
+		self.assertTrue(isinstance(self.profile_one,Profile))
+		#User.objects.all().delete()
+
+	def test_retrieve_profiles(self):
+		'''
+		retrieves all profiles from the db
+		'''
+		all_profiles = Profile.retrieve_profiles()
+		profiles = Profile.objects.all()
+		self.assertTrue(len(all_profiles) == len(profiles))
+
+
+class TagsTestClass(TestCase):
+	'''
+	test case for Tags class
+	'''
+	def setUp(self):
+		'''
+		set up method
+		'''
+		self.tag_one = Tags(title = 'new')
+
+	def test_instance(self):
+		'''
+		test the method
+		'''
+		self.assertTrue(isinstance(self.tag_one,Tags))
+
+	def test_save_tag(self):
+		'''
+		saves tag to the db
+		'''
+		self.tag_one.save_tag()
+		all_tags = Tags.objects.all()
+		self.assertTrue(len(all_tags) > 0)
+
+
+	def test_delete_tag(self):
+		'''
+		deletes tag from the db
+		'''
+		self.tag_one.save_tag()
+		all_tags = Tags.objects.all()
+		self.tag_one.delete_tag()
+		self.assertTrue(len(all_tags) == 0)
+
+	def test_retrieve_tags(self):
+		'''
+		retrieve all tags from the db
+		'''
+		self.tag_one.save_tag()
+		db_tags = Tags.retrieve_tags()
+		all_tags = Tags.objects.all()
+		self.assertTrue(len(db_tags) == len(all_tags)) 
+
+class PostsTestClass(TestCase):
+	'''
+	test case for posts class
+	'''
+	def setUp(self):
+		'''
+		set up method
+		'''
+		self.post_one = Post(caption = 'Trap back jumping!')
+
+	def test_instance(self):
+		'''
+		test the method
+		'''
+		self.assertTrue(isinstance(self.post_one,Post))	
+
+	def test_retrieve_profile_posts(self):
+		'''
+		test for get posts for a specific profile
+		'''
+		self.neville = User(username = 'nevooronni')#create new user
+		self.neville.save()#save new user
+
+		self.chelsea = User(username = 'chelsea')#create another user
+		self.chelsea.save()
+
+		self.new_profile = Profile(user=self.neville,bio = 'bla bla blab bla')#create profile for user neville
+
+		self.new_post = Post(user=self.neville,caption = 'bla bla bal bla')#create post for nevill profile
+
+		retrieve_profile = Post.retrieve_profile_posts(self.neville.id)#get the profile post for profile neville
+
+		profiles = Post.objects.all()#get all posts 
+
+		self.assertTrue(len(retrieve_profile) == len(profiles))#since only profile exists(one we created) should be equla in lenghth retreived profile method 
+
+	def test_retrieve_posts(self):
+		'''
+		test for retriving post from the db
+		'''
+		all_posts = Post.retrieve_posts()
+		posts = Post.objects.all()
+		self.assertTrue(len(all_posts) == len(posts))
+
+	
+
