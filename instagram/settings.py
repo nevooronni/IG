4061bf6a8c3734b54ee120f69a99ca069cb0d4e7
Neville Oronni <nevooronni@gmail.com>
2017-12-07 14:31:45 +0300

    commented out media files in setting.py file

--- instagram/settings.py
+++ instagram/settings.py
@@ -138,6 +138,6 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
